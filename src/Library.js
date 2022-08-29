ab991d78dadd4d0f8d0ee5c95ea967ba1d7804c2
Angela Wirth <angiewirth92@gmail.com>
2022-08-28 20:16:03 -0400

    finish test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,48 @@
+function createLibrary(name){
+var library = {
+  name: name,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: []
+  }
+}
+return library
+}
 
+function addBook(library, book) {
+ if(book.genre === 'fantasy') {
+  library.shelves.fantasy.push(book)
+ }
+ else if (book.genre === 'fiction') {
+  library.shelves.fiction.push(book)
+ }
+ else if (book.genre === 'nonfiction') {
+  library.shelves.nonfiction.push(book)
+ }
+}
+
+function checkoutBook(library, bookTitle, genre) {
+var  shelf = library.shelves[genre]
+var checkedOut = false
+
+for(var i = 0; i < shelf.length; i++) {
+  if(bookTitle === shelf[i].title) {
+    shelf.splice(i,1) 
+    checkedOut = true
+  }
+}
+
+if (checkedOut == true) {
+  return `You have now checkout out ${bookTitle} from the ${library.name}}`
+}
+if (checkedOut === false) {
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+  }
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
